aa092913e4570fbc7202b0b1ab528cb7fa432617
Munta <146889498+Bruteforce777@users.noreply.github.com>
2025-10-21 16:23:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, request,redirect,session,url_for
 import sqlite3
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = "Jamal100%"
+app.secret_key = os.getenv("SECRET_KEY")
 
 DB_NAME = "database.db"
 
